9d46799d9466bd794ca15d70d65f1a34451a655e
adesantos <adelasantos07@gmail.com>
2020-10-12 23:56:57 +0000

    counter

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,37 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from "react";
 
 //create your first component
 export function Home() {
+	const [time, setTime] = useState(0);
+	const timeString = time.toString();
+
+	useEffect(() => {
+		setInterval(() => setTime(new Date().getSeconds()), 1000);
+	}, []);
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div id="clock">
+			<div>
+				<i className={"fas fa-clock"} />
+			</div>
+			<div>
+				<p>0</p>
+			</div>
+			<div>
+				<p>0</p>
+			</div>
+			<div>
+				<p>0</p>
+			</div>
+			<div>
+				<p>0</p>
+			</div>
+			<div>
+				<p>{timeString.length > 1 ? timeString[0] : "0"}</p>
+			</div>
+			<div>
+				<p>{timeString.length > 1 ? timeString[1] : timeString}</p>
+			</div>
 		</div>
 	);
 }
